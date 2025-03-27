3676316b0e469d23d8f08bc7dc1c9c3a1ee0264e
chucho <bohdan.tarverdiiev@gmail.com>
2025-03-28 01:49:28 +0200

    navbar changes

--- src/icp-art-gallery-frontend/src/components/@navbar/Navbar.tsx
+++ src/icp-art-gallery-frontend/src/components/@navbar/Navbar.tsx
@@ -5,10 +5,10 @@ import PlugLogin from '../@wallet-login/PlugLogin';
 export const Navbar = () => {
   return (
     <div className="head-glass flex">
-      <div className="head-text">
+      <div className="head-text flex-1">
         <a href="/">.gallery</a>
       </div>
-      <div className="head-text ml-auto">
+      <div className="head-text flex justify-end">
         <PlugLogin />
       </div>
     </div>
