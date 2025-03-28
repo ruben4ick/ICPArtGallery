ef3dd682375fdb8212ef3b3969f94e5974a80d07
chucho <bohdan.tarverdiiev@gmail.com>
2025-03-28 13:25:24 +0200

    prettier fix

--- src/icp-art-gallery-frontend/src/components/@gallery/Card/Card.tsx
+++ src/icp-art-gallery-frontend/src/components/@gallery/Card/Card.tsx
@@ -2,49 +2,55 @@ import '../../../index.scss';
 import './style.scss';
 import CardProps from '../interfaces/card-props';
 import { ThumbsUp, ThumbsDown } from 'lucide-react';
-import React, {useState} from 'react';
-import CardModal from "../CardModal/CardModal";
+import React, { useState } from 'react';
+import CardModal from '../CardModal/CardModal';
 
 // TODO: hook for like
 // TODO: hook for dislike
 // TODO: hook for details
 
 export const Card: React.FC<{ children: CardProps }> = ({ children }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    return (
-        <>
-            <div className="glass w-[380px] rounded-lg overflow-hidden flex flex-col">
-                <img
-                    alt="card img"
-                    className="mt-[4%] mx-[4%] w-[92%] h-auto object-cover rounded-lg"
-                    src={children.imageLink}
-                />
-                <div className="title pl-[3%] mt-[10px]">
-                    {children.name ? <span>{children.name}</span> : <span>.untitled</span>}
-                </div>
-                {/*TODO: add hooks for likes and dislikes*/}
-                <div className="flex items-center justify-between gap-4 mt-[8px]">
-                    <button className="card-btn mx-[4%]" type="button">
-                        <ThumbsUp size={20} />
-                    </button>
-                    <span className="like-percent">
-                        {children.like_percentage !== null ? `${children.like_percentage}%` : '_null'}
-                    </span>
-                    <button className="card-btn mx-[4%]" type="button">
-                        <ThumbsDown size={20} />
-                    </button>
-                </div>
-                {/*TODO: add hook for details*/}
-                <button className="card-btn details-btn mt-[16px] mb-[4%] mx-[4%]" onClick={() => setIsModalOpen(true)} type="button">
-                    .details
-                </button>
-            </div>
+  return (
+    <>
+      <div className="glass w-[380px] rounded-lg overflow-hidden flex flex-col">
+        <img
+          alt="card img"
+          className="mt-[4%] mx-[4%] w-[92%] h-auto object-cover rounded-lg"
+          src={children.imageLink}
+        />
+        <div className="title pl-[3%] mt-[10px]">
+          {children.name ? <span>{children.name}</span> : <span>.untitled</span>}
+        </div>
+        {/*TODO: add hooks for likes and dislikes*/}
+        <div className="flex items-center justify-between gap-4 mt-[8px]">
+          <button className="card-btn mx-[4%]" type="button">
+            <ThumbsUp size={20} />
+          </button>
+          <span className="like-percent">
+            {children.like_percentage !== null ? `${children.like_percentage}%` : '_null'}
+          </span>
+          <button className="card-btn mx-[4%]" type="button">
+            <ThumbsDown size={20} />
+          </button>
+        </div>
+        {/*TODO: add hook for details*/}
+        <button
+          className="card-btn details-btn mt-[16px] mb-[4%] mx-[4%]"
+          onClick={() => setIsModalOpen(true)}
+          type="button"
+        >
+          .details
+        </button>
+      </div>
 
-            {isModalOpen ? <div className="fixed w-full h-full flex items-center justify-center inset-0 z-99">
-                    <CardModal onClose={() => setIsModalOpen(false)}>{children}</CardModal>
-                </div> : null}
-        </>
+      {isModalOpen ? (
+        <div className="fixed w-full h-full flex items-center justify-center inset-0 z-99">
+          <CardModal onClose={() => setIsModalOpen(false)}>{children}</CardModal>
+        </div>
+      ) : null}
+    </>
   );
 };
 
--- src/icp-art-gallery-frontend/src/components/@gallery/CardModal/CardModal.tsx
+++ src/icp-art-gallery-frontend/src/components/@gallery/CardModal/CardModal.tsx
@@ -5,8 +5,8 @@ import { ThumbsUp, ThumbsDown, X } from 'lucide-react';
 import React from 'react';
 
 interface CardModalProps {
-    children: CardProps;
-    onClose: () => void;
+  children: CardProps;
+  onClose: () => void;
 }
 
 // TODO: hook for like
@@ -14,40 +14,45 @@ interface CardModalProps {
 // TODO: hook for details
 
 export const CardModal: React.FC<CardModalProps> = ({ children, onClose }) => {
-    return (
-        <div className="glass modal-glass w-[960px] rounded-lg overflow-hidden p-6 relative grid grid-cols-[60%_auto] grid-rows-[auto_auto] gap-4">
-            <button
-                aria-label="Close modal"
-                className="card-btn modal-btn modal-close-btn absolute top-2 right-2 text-white p-2 hover:bg-white/10 transition z-10"
-                type="button"
-                onClick={onClose}>
-                <X size={24}/>
-            </button>
-            <img
-                alt="card img"
-                className="w-full h-auto object-cover rounded-lg col-start-1 row-start-1"
-                src={children.imageLink}
-            />
-            {/*TODO: for description or some other text*/}
-            <div className="modal-description col-start-2 row-end-2 pt-5">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus distinctio doloremque est illum incidunt nisi sequi sint, vitae. Animi, beatae doloremque ea ex fuga fugiat nisi porro temporibus tenetur vel.</p>
-            </div>
-            <div className="modal-title col-start-1 row-start-2 self-start mt-[10px]">
-                {children.name ? <span>{children.name}</span> : <span>.untitled</span>}
-            </div>
-            <div className="col-start-2 row-start-2 flex items-center justify-end gap-4 self-center mt-[10px]">
-                <button className="card-btn modal-btn" type="button">
-                    <ThumbsUp size={20}/>
-                </button>
-                <span className="like-percent modal-like-percent">
-                    {children.like_percentage !== null ? `${children.like_percentage}%` : '_null'}
-                </span>
-                <button className="card-btn modal-btn" type="button">
-                    <ThumbsDown size={20}/>
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="glass modal-glass w-[960px] rounded-lg overflow-hidden p-6 relative grid grid-cols-[60%_auto] grid-rows-[auto_auto] gap-4">
+      <button
+        aria-label="Close modal"
+        className="card-btn modal-btn modal-close-btn absolute top-2 right-2 text-white p-2 hover:bg-white/10 transition z-10"
+        onClick={onClose}
+        type="button"
+      >
+        <X size={24} />
+      </button>
+      <img
+        alt="card img"
+        className="w-full h-auto object-cover rounded-lg col-start-1 row-start-1"
+        src={children.imageLink}
+      />
+      {/*TODO: for description or some other text*/}
+      <div className="modal-description col-start-2 row-end-2 pt-5">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus distinctio doloremque
+          est illum incidunt nisi sequi sint, vitae. Animi, beatae doloremque ea ex fuga fugiat nisi
+          porro temporibus tenetur vel.
+        </p>
+      </div>
+      <div className="modal-title col-start-1 row-start-2 self-start mt-[10px]">
+        {children.name ? <span>{children.name}</span> : <span>.untitled</span>}
+      </div>
+      <div className="col-start-2 row-start-2 flex items-center justify-end gap-4 self-center mt-[10px]">
+        <button className="card-btn modal-btn" type="button">
+          <ThumbsUp size={20} />
+        </button>
+        <span className="like-percent modal-like-percent">
+          {children.like_percentage !== null ? `${children.like_percentage}%` : '_null'}
+        </span>
+        <button className="card-btn modal-btn" type="button">
+          <ThumbsDown size={20} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CardModal;
